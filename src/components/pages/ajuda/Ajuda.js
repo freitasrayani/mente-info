b856f70fb311eb651c1e2f0925140bd164e1edca
Wendell Matheus <wendellmatheus77@gmail.com>
2020-10-16 13:38:56 -0300

    Add upgrade no layout

--- src/components/pages/ajuda/Ajuda.js
+++ src/components/pages/ajuda/Ajuda.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './ajuda.css'
+import './ajuda.css';
+import cellphone from './img-ajuda/cellphone.jpg';
+import { Link } from 'react-router-dom';
 
 const Ajuda = () => (
-    <div className="ajuda-container">
 
-    </div>
-)
 
-export default Ajuda
+    <section className="box-ajuda">
+
+        <div className="box-img-ajuda">
+            <img className="img-ajuda" src={cellphone} alt="Imagem de um Smatphone" />
+        </div>
+
+        <div className="box-description">
+
+            <div className="box-title">
+                <h1 class="title-ajuda"> Busque ajuda com a plataforma Mente-Care </h1>
+            </div>
+
+            <div className="box-mente">
+                <p className="description">O mente care é uma App gratuito, onde você consegue ajuda com especialistas.</p>
+            </div>
+
+            <div className="box-button">
+                <Link className="button-ajuda" to="/">Buscar Ajuda</Link>
+            </div>
+        </div>
+
+    </section>
+
+);
+
+export default Ajuda;
